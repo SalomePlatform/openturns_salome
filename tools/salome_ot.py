1a80107cdb2f5674615ef35c3b7b3a9c4937af46
Ovidiu Mircescu <ovidiu.mircescu@edf.fr>
2019-03-15 16:24:21 +0100

    New way to create an OtStudy.

--- tools/salome_ot.py
+++ tools/salome_ot.py
@@ -29,6 +29,7 @@ def loadYacsPyStudy(pyFile,
                     yacs_file = ""):
   """Open OPENTURNS module and create a study using a YACS model from a python
      file.
+     This is the old way to create an OtStudy. getYacsPyStudy should be prefered.
   """
   if not yacs_file:
     yacs_file = os.path.join(os.path.dirname(pyFile), "yacs.xml")
@@ -43,3 +44,34 @@ def loadYacsPyStudy(pyFile,
     physicalModel = otguibase.YACSPhysicalModel(model_name, file_text)
     otStudy.add(physicalModel)
   return errors
+
+def getYacsPyStudy(pyScript, study_name = None, model_name = None):
+  """ Open OPENTURNS module and create a study using a YACS model from a python
+      script.
+      Parameters:
+        - pyScript : python script as text
+        - study_name : if None, use a default name
+        - model_name : if None, use a default name
+      Return:
+        - otStudy
+        - physical model
+
+      You can set the job parameters this way:
+      study, model = salome_ot.getYacsPyStudy(myScript)
+      model.jobParameters().salome_parameters.in_files = ["/one/file", "/another/file"]
+
+      You can find all the options of 'salome_parameters' in the structure
+      JobParameters from SALOME_Launcher.idl (KERNEL module).
+  """
+  sgPyQt = SalomePyQt.SalomePyQt()
+  sgPyQt.activateModule("OpenTURNS")
+  if study_name is None:
+    study_name = "MyStudy"
+  study_name = otguibase.Study.GetAvailableName(study_name)
+  otStudy = otguibase.Study(study_name)
+  otguibase.Study.Add(otStudy)
+  if model_name is None:
+    model_name = "PhysicalModel"
+  physicalModel = otguibase.YACSPhysicalModel(model_name, pyScript)
+  otStudy.add(physicalModel)
+  return otStudy, physicalModel
